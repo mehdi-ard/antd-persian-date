49fc64f3fac875e2955550f13614ce32408917c4
Mehdi Ardakani <mehdi.ardakani@live.com>
2026-01-27 15:07:41 +0330

    fix config dayjs

--- src/jalali/jalali-locale-listener-hook.tsx
+++ src/jalali/jalali-locale-listener-hook.tsx
@@ -1,18 +1,18 @@
-import React, { useContext } from "react";
 import { ConfigProvider } from "antd";
 import dayjs from "dayjs";
-import jalaliday from "jalaliday";
 import calendar from "dayjs/plugin/calendar";
+import jalaliday from "jalaliday";
+import React, { useContext } from "react";
 
 /**
  * Subscribes to the ConfigProvider locale changes and updates the dayjs calendar based on current locale.
  */
 
-dayjs.extend(calendar);
-dayjs.extend(jalaliday);
-
 export const useJalaliLocaleListener = () => {
   const { locale } = useContext(ConfigProvider.ConfigContext);
+  dayjs.extend(calendar);
+  dayjs.extend(jalaliday);
+
   React.useEffect(() => {
     if (locale?.locale == "fa") {
       dayjs["calendar"]?.("jalali");
@@ -21,4 +21,3 @@ export const useJalaliLocaleListener = () => {
     }
   }, [locale]);
 };
-
